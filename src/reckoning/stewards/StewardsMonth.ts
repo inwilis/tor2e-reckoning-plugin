83f1460e0d89c0285b492438556cdb72f5247bc9
inwilis <>
2023-04-12 22:40:17 +0200

    fixed seasons

--- src/reckoning/stewards/StewardsMonth.ts
+++ src/reckoning/stewards/StewardsMonth.ts
@@ -1,11 +1,26 @@
 export enum StewardsMonth {
+    /**
+     * Winter solstice intercalary
+     */
     YESTARE = "YESTARE",
     NARVINYE = "NARVINYE", NENIME = "NENIME", SULIME = "SULIME",
+    /**
+     * Spring equinox intercalary
+     */
     TUILERE = "TUILERE",
     VIRESSE = "VIRESSE", LOTESSE = "LOTESSE", NARIE = "NARIE",
+    /**
+     * Summer solstice intercalary
+     */
     LOENDE = "LOENDE",
+    /**
+     * Summer solstice leap intercalary
+     */
     ENDERE = "ENDERE",
     CERMIE = "CERMIE", URIME = "URIME", YAVANNIE = "YAVANNIE",
+    /**
+     * Autumn equinox intercalary
+     */
     YAVIERE = "YAVIERE",
     NARQUELIE = "NARQUELIE", HISIME = "HISIME", RINGARE = "RINGARE",
     METTARE = "METTARE"
